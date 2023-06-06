50b40e5c0396c69a46b5aa9fc9d7dd7f8cfd80d7
Garpusha <theflash@yandex.ru>
2023-06-06 21:45:42 +0300

    models and csv

--- 2.1-databases/work_with_database/phones/management/commands/import_phones.py
+++ 2.1-databases/work_with_database/phones/management/commands/import_phones.py
@@ -12,6 +12,13 @@ class Command(BaseCommand):
         with open('phones.csv', 'r') as file:
             phones = list(csv.DictReader(file, delimiter=';'))
 
-        for phone in phones:
-            # TODO: Добавьте сохранение модели
-            pass
+ for phone in phones:
+            my_record = Phone(
+                name=phone['name'],
+                image=phone['image'] ,
+                price=int(phone['price']),
+                release_date=datetime.strptime(phone['release_date'], "%Y-%m-%d"),
+                lte_exists=phone['lte_exists'],
+                slug=phone['name'].replace(' ', '_'),
+                )
+            my_record.save()
