951926419bfa58fc0983c1c33ad3f6c61e9053f3
Garpusha <theflash@yandex.ru>
2023-06-07 11:36:15 +0300

    ORM_1 HW

--- 2.1-databases/work_with_database/phones/management/commands/import_phones.py
+++ 2.1-databases/work_with_database/phones/management/commands/import_phones.py
@@ -2,6 +2,7 @@ import csv
 
 from django.core.management.base import BaseCommand
 from phones.models import Phone
+from datetime import datetime
 
 
 class Command(BaseCommand):
@@ -12,13 +13,13 @@ class Command(BaseCommand):
         with open('phones.csv', 'r') as file:
             phones = list(csv.DictReader(file, delimiter=';'))
 
- for phone in phones:
-            my_record = Phone(
-                name=phone['name'],
-                image=phone['image'] ,
-                price=int(phone['price']),
-                release_date=datetime.strptime(phone['release_date'], "%Y-%m-%d"),
-                lte_exists=phone['lte_exists'],
-                slug=phone['name'].replace(' ', '_'),
-                )
-            my_record.save()
+        for phone in phones:
+                    my_record = Phone(
+                        name=phone['name'],
+                        image=phone['image'] ,
+                        price=int(phone['price']),
+                        release_date=datetime.strptime(phone['release_date'], "%Y-%m-%d"),
+                        lte_exists=phone['lte_exists'],
+                        slug=phone['name'].replace(' ', '_').lower(),
+                        )
+                    my_record.save()
