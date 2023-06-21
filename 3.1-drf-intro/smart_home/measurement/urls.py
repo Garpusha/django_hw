61c0f67ca3e36cb58b6576d3fd6bc33e1b125463
Garpusha <theflash@yandex.ru>
2023-06-21 18:24:41 +0300

    HW 3_1

--- 3.1-drf-intro/smart_home/measurement/urls.py
+++ 3.1-drf-intro/smart_home/measurement/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from measurement.views import SensorsView, SensorView, SensorUpdate, AddMeasurement
 
 urlpatterns = [
     # TODO: зарегистрируйте необходимые маршруты
+    path("sensors/", SensorsView.as_view()),
+    path("sensorupdate/<pk>/", SensorUpdate.as_view()),
+    path("sensors/<pk>/", SensorView.as_view()),
+    path("measurements/", AddMeasurement.as_view()),
 ]
--- 3.1-drf-intro/smart_home/measurement/views.py
+++ 3.1-drf-intro/smart_home/measurement/views.py
@@ -1,2 +1,54 @@
 # TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
 # TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+
+from rest_framework.generics import (
+    ListAPIView,
+    RetrieveAPIView,
+    RetrieveUpdateDestroyAPIView,
+)
+from .models import Sensor, Measurement
+from .serializers import SensorDetailSerializer, MeasurementSerializer
+from rest_framework.response import Response
+
+
+class SensorsView(ListAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorDetailSerializer
+
+    def post(self, request):
+        Sensor(
+            name=request.data["name"], description=request.data["description"]
+        ).save()
+        return Response("Post OK")
+
+
+# @api_view(['PATCH'])
+class SensorUpdate(RetrieveUpdateDestroyAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorDetailSerializer
+
+    def delete(self, request, pk):
+        Sensor(id=pk).delete()
+        return Response("Delete OK")
+
+    def patch(self, request, pk):
+        Sensor(id=pk, description=request.data["description"]).save()
+        return Response("Patch OK")
+
+
+class SensorView(RetrieveAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorDetailSerializer
+
+
+class AddMeasurement(ListAPIView):
+    queryset = Measurement.objects.all()
+    serializer_class = MeasurementSerializer
+
+    def post(self, request):
+        s_id = request.data["sensor"]
+        sensor = Sensor.objects.get(id=s_id)
+        Measurement(
+            sensor=sensor, temperature=float(request.data["temperature"])
+        ).save()
+        return Response("Post OK")
