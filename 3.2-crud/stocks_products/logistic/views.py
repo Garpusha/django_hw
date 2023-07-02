fc6e619507bc429e31699fdb69909dd309bc6f4f
Garpusha <theflash@yandex.ru>
2023-07-02 14:14:12 +0300

    CRUD

--- 3.2-crud/stocks_products/logistic/views.py
+++ 3.2-crud/stocks_products/logistic/views.py
@@ -7,10 +7,15 @@ from logistic.serializers import ProductSerializer, StockSerializer
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    search_fields = ['title', 'description']
+    ordering_fields = ['id', 'title']
     # при необходимости добавьте параметры фильтрации
 
 
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
+    filterset_fields = ['id']
+    ordering_fields = ['id', 'address']
+
     # при необходимости добавьте параметры фильтрации
